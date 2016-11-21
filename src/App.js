2d17a67e650dcfb71bdce11ee7bb61660d34402d
Anastassios Martakos <anastassios.martakos@gmail.com>
2016-11-21 22:12:12 +0100

    You can now press on the fields and also see a history

--- src/App.js
+++ src/App.js
@@ -2,41 +2,78 @@ import React, { Component } from 'react';
 import './App.css';
 
 const X = "X";
-const O = "Y";
+const O = "O";
 
 class Square extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      value : null
-    }
-  }
-
   render() {
-    return <div className="square">{ this.state.value }</div>
+    return <div className="square" onClick={ () => this.props.onClick() }><p>{ this.props.value }</p></div>
   }
 }
 
-class BoardRow extends Component {
+class Info extends Component {
   render() {
+    let items = this.props.histroy.map(x => <div><a href="#">{x.nextState === X ? O : X}</a></div>)
     return (
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+      <div className="info">
+        {items}
       </div>
-    );
+    )
   }
 }
 
 class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      nextState: X,
+      histroy: []
+    }
+
+    this.onClick = this.onClick.bind(this)
+  }
+
+  renderSquare(i) {
+    return <Square onClick={() => this.onClick(i)} value={this.state.squares[i]} />
+  }
+
+  onClick(i) {
+    this.setState((prevState) => {
+      let newState = prevState;
+
+      if(newState.squares[i] === null) {
+        newState.squares[i] = prevState.nextState
+        newState.histroy.push(newState)
+      }
+
+      newState.nextState = newState.nextState === X ? O : X
+
+      return newState
+    })
+  }
+
   render() {
     return (
-      <div className="board">
-        <BoardRow />
-        <BoardRow />
-        <BoardRow />
+      <div>
+        <div className="board">
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+
+        <Info histroy={this.state.histroy}/>
       </div>
     );
   }
