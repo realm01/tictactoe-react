fb5188bb4fe0b3f97c4b42a5b3820563ebd9fece
Anastassios Martakos <anastassios.martakos@gmail.com>
2016-11-25 07:56:27 +0000

    Declaring winner

--- src/App.js
+++ src/App.js
@@ -40,6 +40,27 @@ class Board extends Component {
     this.onHistoryClick = this.onHistoryClick.bind(this)
   }
 
+  declaringWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (this.state.squares[a] && this.state.squares[a] === this.state.squares[b] && this.state.squares[a] === this.state.squares[c]) {
+        return <p>Winner: {this.state.squares[a]}</p>
+      }
+    }
+    return null;
+    }
+
   renderSquare(i) {
     return <Square onClick={() => this.onClick(i)} value={this.state.squares[i]} />
   }
@@ -93,6 +114,11 @@ class Board extends Component {
         </div>
 
         <Info histroy={this.state.histroy} onClick={this.onHistoryClick}/>
+        
+        <div>
+          <p>Next Player: {this.state.nextState}</p>
+          {this.declaringWinner()}
+        </div>
       </div>
     );
   }
